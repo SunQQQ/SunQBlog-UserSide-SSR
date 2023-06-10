876d60a952a11a0fe07cac232507a7768b34254b
sunquan <1585437938@qq.com>
2023-06-10 15:16:23 +0800

    复制博客代码

--- final/app.js
+++
@@ -1,6 +0,0 @@
-import vue from 'vue';
-
-export let ssrApp = new vue({
-    data: () => ({ count: 1 }),
-    template: `<button @click="count++">{{ count }}</button>`
-})
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="my-scrollbar">
+        <TopBar></TopBar>
+        <router-view></router-view>
+        <Footer></Footer>
+        <Tips></Tips>
+        <Loading></Loading>
+        <CanvasBac></CanvasBac>
+        <AirPlane></AirPlane>
+        <!-- <spring-festival></spring-festival> -->
+    </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import TopBar from './components/BrotherCompnent/TopBar'
+  import Footer from './components/BrotherCompnent/Footer'
+  import Tips from './components/BrotherCompnent/Tips'
+  import Loading from './components/BrotherCompnent/Loading'
+  import CanvasBac from './components/BrotherCompnent/CanvasBac'
+  import AirPlane from './components/BrotherCompnent/Airplane'
+  // import springFestival from '@/components/BrotherCompnent/theme/springFestival'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+    components: {
+      TopBar: TopBar,
+      Footer:Footer,
+      Tips: Tips,
+      Loading:Loading,
+      CanvasBac:CanvasBac,
+      AirPlane:AirPlane,
+      // springFestival:springFestival
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped lang="less">
+.my-scrollbar::-webkit-scrollbar {/*滚动条整体样式*/
+
+  width: 10px;     /*高宽分别对应横竖滚动条的尺寸*/
+
+  height: 1px;
+
+}
+
+.my-scrollbar::-webkit-scrollbar-thumb {/*滚动条里面小方块*/
+
+  border-radius: 10px;
+
+  -webkit-box-shadow: inset 0 0 5px rgba(0,0,0,0.2);
+
+  background: #535353;
+
+}
+
+.my-scrollbar::-webkit-scrollbar-track {/*滚动条里面轨道*/
+
+  -webkit-box-shadow: inset 0 0 5px rgba(0,0,0,0.2);
+
+  border-radius: 10px;
+
+  background: #EDEDED;
+
 }
 </style>
